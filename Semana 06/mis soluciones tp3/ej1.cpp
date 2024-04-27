974afc30229163304a4e5211ad4cc62b93786c28
Micaela Reyes <reyessmicaela@gmail.com>
2024-04-27 08:49:24 -0300

    solicion ejercicio 1

---
+++ Semana 06/mis soluciones tp3/ej1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+//1
+//Hacer un programa para mostrar por pantalla los números del 1 al 10.
+//Importante: El programa no tiene ningún ingreso de datos.
+
+int main (){
+    for( int i=1; i<11; i++){
+        cout << i << endl;
+    }
+    return 0;
+}
